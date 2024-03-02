d42256f90e45887e0330d4649a1efc9039e55d37
Andres <freetrialandy@gmail.com>
2024-03-01 18:33:52 -0800

    bugs route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,13 +11,13 @@ export default function Header() {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a href='#/'>Live Fixtures</a></li>
+                            <li><a href='/OnSideFutbol/#/'>Live Fixtures</a></li>
                             <li><a href='#/upcoming-matches/'>Upcoming Fixtures</a></li>
                         </ul>
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <Link to={'/'} className="btn btn-ghost text-xl">OnSide Fútbol</Link>
+                    <Link to={'/OnSideFutbol/#/'} className="btn btn-ghost text-xl">OnSide Fútbol</Link>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
